d38eae9db02d5ddf3762e0394b9344fa882a08e2
dhpe <dhpe@brasilcap.com.br>
2025-01-03 16:08:09 -0300

    feat: adaptando API para AWS Lambda

--- src/server.ts
+++ src/server.ts
@@ -10,53 +10,55 @@ import {
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { UserRoutes } from "./routes/user.routes";
-
-const app = fastify().withTypeProvider<ZodTypeProvider>();
-
 import { AuthRoutes } from "./routes/auth.routes";
 import { PostRoutes } from "./routes/post.routes";
 import { CommentRoutes } from "./routes/comment.routes";
 import { RoleRoutes } from "./routes/role.routes";
+import AWSLambda from "@fastify/aws-lambda";
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+export const handler = AWSLambda(
+  (() => {
+    const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.register(fastifyCors, { origin: "*" });
+    app.setValidatorCompiler(validatorCompiler);
+    app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "Blog API",
-      version: "1.0.0",
-    },
-  },
-  transform: jsonSchemaTransform,
-});
+    app.register(fastifyCors, { origin: "*" });
 
-app.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
-});
+    app.register(fastifySwagger, {
+      openapi: {
+        info: {
+          title: "Blog API",
+          version: "1.0.0",
+        },
+      },
+      transform: jsonSchemaTransform,
+    });
 
-app.register(UserRoutes.routes, {
-  prefix: "/api/user",
-});
+    app.register(fastifySwaggerUi, {
+      routePrefix: "/docs",
+    });
 
-app.register(AuthRoutes.routes, {
-  prefix: "/api/auth",
-});
+    app.register(UserRoutes.routes, {
+      prefix: "/api/user",
+    });
 
-app.register(PostRoutes.routes, {
-  prefix: "/api/post",
-});
+    app.register(AuthRoutes.routes, {
+      prefix: "/api/auth",
+    });
 
-app.register(CommentRoutes.routes, {
-  prefix: "/api/comment",
-});
+    app.register(PostRoutes.routes, {
+      prefix: "/api/post",
+    });
 
-app.register(RoleRoutes.routes, {
-  prefix: "/api/role",
-});
+    app.register(CommentRoutes.routes, {
+      prefix: "/api/comment",
+    });
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP Server is running...");
-});
+    app.register(RoleRoutes.routes, {
+      prefix: "/api/role",
+    });
+
+    return app;
+  })()
+);
